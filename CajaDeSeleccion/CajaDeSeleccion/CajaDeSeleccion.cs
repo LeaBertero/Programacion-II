a8819dc2fc257c70f130708312ab904aa8504c2a
Leandro Bertero <leabertero@hotmail.com>
2022-05-16 09:45:41 -0300

    diseño

---
+++ CajaDeSeleccion/CajaDeSeleccion/CajaDeSeleccion.cs
@@ -0,0 +1,20 @@
+namespace CajaDeSeleccion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void BtLimpiar_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
